253f8ff33931c2db43d3e5228bcaba16be23f899
MrCowsen <58846830+EBrandDV@users.noreply.github.com>
2023-05-04 11:15:10 +0200

    notebook progress and density fix

--- queries.py
+++ queries.py
@@ -146,7 +146,7 @@ def q_density(graph):
                     }}
                     }}
                 {{
-                select (count(*) as ?E) where {{?s ?p ?o}}
+                select (count(*) as ?E) where {{ graph {{?s ?p ?o}}}}
                 }}
             }}
             ''' 

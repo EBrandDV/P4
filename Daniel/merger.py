197613e71f2bb283d90afffce9231319384a501e
SweetAndJuicy <92367976+SweetAndJuicy@users.noreply.github.com>
2023-05-04 12:01:54 +0200

    Added density data

---
+++ Daniel/merger.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+big = r'D:\Andre programmer\Github\Github_Repo\P4\Data\struct4.csv'
+small = r'D:\Andre programmer\Github\Github_Repo\P4\Data\Density.csv'
+
+
+
+df = pd.read_csv(big)
+
+print(df)
+
